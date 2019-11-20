27afb41d1036469c2ff738854d486bad6b13f8b8
hadouani <hadouani.z.rcr@gmail.com>
2019-11-20 21:58:30 +0100

    first commit

---
+++ src/main/java/org/sid/kafka/springbootkafka/SpringBootKafkaApplication.java
@@ -0,0 +1,15 @@
+package org.sid.kafka.springbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@SpringBootApplication
+
+public class SpringBootKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaApplication.class, args);
+    }
+
+}
